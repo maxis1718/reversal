5bb570e1f9bc4e90ef91a1490ace842c7077b775
Maxis Kao <maxiskao@yahoo-inc.com>
2015-12-07 02:01:25 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='reversal',
+      version='0.1',
+      description='A furious war between trading indices',
+      url='https://github.com/maxis1718/reversal',
+      author='Maxis Kao',
+      author_email='maxis1718@gamil.com',
+      license='MIT',
+      packages=['reversal'],
+      zip_safe=False)
