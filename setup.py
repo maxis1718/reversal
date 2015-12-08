88cc4f918eeab00f855bfca4c8165ed04c102879
Maxis Kao <maxiskao@yahoo-inc.com>
2015-12-09 01:08:23 +0800

    refactor setup

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
+from setuptools import find_packages
 from setuptools import setup
 
-setup(name='reversal',
-      version='0.1',
-      description='A furious war between trading indices',
-      url='https://github.com/maxis1718/reversal',
-      author='Maxis Kao',
-      author_email='maxis1718@gamil.com',
-      license='MIT',
-      packages=['reversal'],
-      zip_safe=False)
+setup(
+    name='reversal',
+    description='A furious war between trading indices',
+    url='https://github.com/maxis1718/reversal',
+    version='0.1',
+
+    author='Maxis Kao',
+    author_email='maxis1718@gamil.com',
+
+    license='MIT',
+    packages=find_packages('.', exclude=('tests*', 'testing*')),
+    zip_safe=False
+)
